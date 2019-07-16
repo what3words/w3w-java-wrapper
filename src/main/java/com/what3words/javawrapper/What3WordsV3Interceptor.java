25f98c627a9e61a7f30154cc45bb419126592c29
Simon Barnett <simon@what3words.com>
2019-07-16 10:42:43 +0100

    Set 'User-Agent' header

--- src/main/java/com/what3words/javawrapper/What3WordsV3Interceptor.java
+++ src/main/java/com/what3words/javawrapper/What3WordsV3Interceptor.java
@@ -1,6 +1,8 @@
 package com.what3words.javawrapper;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 import okhttp3.Interceptor;
 import okhttp3.Request;
@@ -8,9 +10,29 @@ import okhttp3.Response;
 
 public class What3WordsV3Interceptor implements Interceptor {
     private String apiKey;
+    private String userAgent;
 
     public What3WordsV3Interceptor(String apiKey) {
         this.apiKey = apiKey;
+        this.userAgent = getUserAgent();
+    }
+    
+    private String getUserAgent() {
+        return "what3words-Java/" + getVersion() + " (Java " + System.getProperty( "java.version" ) + "; "
+            + System.getProperty( "os.name" ) + " " + System.getProperty( "os.version" ) + ")";
+    }
+
+    private String getVersion() {
+        Properties props = new Properties();
+
+        try (InputStream is = getClass().getResourceAsStream("/version.properties")) {
+            if (is != null) {
+                props.load(is);
+            }
+        } catch (IOException e) {
+        }
+
+        return props.getProperty("version", "unknown-version");
     }
 
     public Response intercept(Chain chain) throws IOException {
@@ -21,6 +43,7 @@ public class What3WordsV3Interceptor implements Interceptor {
         // set required content type, api key and request specific API version
         builder.header(What3WordsV3.HEADER_CONTENT_TYPE, What3WordsV3.CONTENT_TYPE_JSON);
         builder.header(What3WordsV3.HEADER_WHAT3WORDS_API_KEY, this.apiKey);
+        builder.header(What3WordsV3.HEADER_USER_AGENT, this.userAgent);
 
         return chain.proceed(builder.build());
     }
