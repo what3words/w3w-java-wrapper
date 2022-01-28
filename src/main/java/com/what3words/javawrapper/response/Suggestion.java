08bbb0ba493b995794b71378a1e5d60b210dacd6
Mani <emanuel@what3words.com>
2022-01-28 10:47:27 +0000

    added Suggestion constructor, removed public setters SuggestionWithCoordinates

--- src/main/java/com/what3words/javawrapper/response/Suggestion.java
+++ src/main/java/com/what3words/javawrapper/response/Suggestion.java
@@ -8,6 +8,15 @@ public class Suggestion {
     private int rank;
     private String language;
 
+    public Suggestion(String words, String nearestPlace, String country, Integer distanceToFocusKm, int rank, String language) {
+        this.words = words;
+        this.nearestPlace = nearestPlace;
+        this.country = country;
+        this.distanceToFocusKm = distanceToFocusKm;
+        this.rank = rank;
+        this.language = language;
+    }
+
     public String getCountry() {
         return country;
     }
--- src/main/java/com/what3words/javawrapper/response/SuggestionWithCoordinates.java
+++ src/main/java/com/what3words/javawrapper/response/SuggestionWithCoordinates.java
@@ -42,77 +42,39 @@ public class SuggestionWithCoordinates {
         square = convertToCoordinates.getSquare();
     }
 
-
     public String getCountry() {
         return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
     }
 
     public Square getSquare() {
         return square;
     }
 
-    public void setSquare(Square square) {
-        this.square = square;
-    }
-
     public String getNearestPlace() {
         return nearestPlace;
-    }
-
-    public void setNearestPlace(String nearestPlace) {
-        this.nearestPlace = nearestPlace;
     }
 
     public Coordinates getCoordinates() {
         return coordinates;
     }
 
-    public void setCoordinates(Coordinates coordinates) {
-        this.coordinates = coordinates;
-    }
-
     public String getWords() {
         return words;
-    }
-
-    public void setWords(String words) {
-        this.words = words;
     }
 
     public String getLanguage() {
         return language;
     }
 
-    public void setLanguage(String language) {
-        this.language = language;
-    }
-
     public String getMap() {
         return map;
-    }
-
-    public void setMap(String map) {
-        this.map = map;
     }
 
     public Integer getDistanceToFocusKm() {
         return distanceToFocusKm;
     }
 
-    public void setDistanceToFocusKm(Integer distanceToFocusKm) {
-        this.distanceToFocusKm = distanceToFocusKm;
-    }
-
     public int getRank() {
         return rank;
     }
-
-    public void setRank(int rank) {
-        this.rank = rank;
-    }
-
 }
