0b6a3cccf97679e03610a90de72fe17691960123
Emanuel Amiguinho <emanuel@what3words.com>
2023-01-23 11:58:30 +0000

    added more testing around missing interface calls

---
+++ src/test/java/com/what3words/javawrapper/AvailableLanguagesTest.java
@@ -0,0 +1,18 @@
+package com.what3words.javawrapper;
+
+import com.what3words.javawrapper.response.AvailableLanguages;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class AvailableLanguagesTest {
+    What3WordsV3 api = new What3WordsV3(System.getenv("W3W_API_KEY"));
+
+    @Test
+    public void validAvailableLanguagesTest() {
+        AvailableLanguages response = api.availableLanguages().execute();
+
+        assertTrue(response.isSuccessful());
+        assertTrue(response.getLanguages().size() > 0);
+    }
+}
